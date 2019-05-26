9c81a4f1280506e71d9bc4f59d437278fc3263b2
Paulo Machado <pauloalexandre3d@gmail.com>
2019-05-26 15:04:12 +0200

    fix: Adding deploy in heroku

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi there!
+          Deploying Heroku
         </a>
       </header>
     </div>
